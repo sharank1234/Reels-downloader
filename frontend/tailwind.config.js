b2dd52ee6db06d0bca6d307d4e3de1960ed5af4a
sharank1234 <karthiksharan208@gmail.com>
2026-08-17 22:15:43 +0530

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        insta: {
+          purple: "#833AB4",
+          pink: "#E1306C",
+          orange: "#F77737",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
